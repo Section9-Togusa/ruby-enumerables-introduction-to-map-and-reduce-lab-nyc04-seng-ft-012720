84802cb0654f737ca0812bea85925f31de3a2da6
Daryl Corbin <corbindac@gmail.com>
2020-01-19 19:21:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,19 +1,9 @@
-def map(arr)
-  n = 0
-  newArr = []
-  while n < arr.length do
-    newArr.push( yield( arr[n] ))
-    n += 1
+def map(s)
+  new = []
+  i = 0
+  while i < s.length
+    new.push(yield(s[i]))
+    i += 1
   end
-  newArr
-end
-
-def reduce(arr, start = 0)
-  n = 0
-  value = start
-  while n < arr.length do
-    value = yield( arr[n], value)
-    n += 1
-  end
-  value != nil ? value : false
+  new
 end
