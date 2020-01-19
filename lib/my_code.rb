2e8d025aa36e4a8ae70af9bb9761bcea5a7db6aa
Daryl Corbin <corbindac@gmail.com>
2020-01-19 19:15:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
-# My Code here....
+def map(array)
+  out = []
+
+  count = 0
+
+
+  while (count < array.count) do
+    out.push(yield(array[count]))
+    count+=1
+  end
+
+
+
+  out
+
+end
+
+
+def reduce(array,*starting)
+  if starting[0]
+    value = starting[0]
+    count = 0
+    else
+    value = array[0]
+    count = 1
+  end
+
+  while count < array.count do
+    value =  yield(value,array[count])
+    count+=1
+  end 
+ value
+end
